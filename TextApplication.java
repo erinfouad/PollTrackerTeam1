a9c6b24cf3b87762b17eb797013a240ddedf164e
noob96master <gabe@demong.net>
2021-11-29 18:08:54 -0700

    Updated methods in text application.

--- TextApplication.java
+++ TextApplication.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * @author gabe
  * @version 0.1
@@ -6,17 +8,78 @@ public class TextApplication {
 	private PollList polls;
 
 	public void displayPollDataBySeat(Poll aPoll) {
-		aPoll.textVisualizationBySeats(this.polls.MAX_STARS_FOR_VISUALIZATION, 0);
+		int seatsPerStar = this.polls.getNumOfSeats() / this.polls.MAX_STARS_FOR_VISUALIZATION; // duplicate from pollList, fix later
+		if (seatsPerStar < 1.0*this.polls.getNumOfSeats() / this.polls.MAX_STARS_FOR_VISUALIZATION) seatsPerStar++;
+		System.out.println(aPoll.textVisualizationBySeats(this.polls.MAX_STARS_FOR_VISUALIZATION, seatsPerStar));
 		return;
 	}
 
-	public void displayPollsBySeat(String[] stuff) {
+	public void displayPollsBySeat(String[] partyNames) {
+		for (Poll aPoll : this.polls.polls) {
+			displayPollDataBySeat(aPoll);
+		}
 		
+		displayPollDataBySeat(polls.getAggregatePoll(partyNames));		
 		return;
 	}
 	
+	public void promptForPollList(String[] partyNamesArray) {
+		Scanner myScanner = new Scanner(System.in);
+		for(int i = 0; i < this.polls.polls.length; i++) {
+			System.out.print("Enter the name of poll " + (i+1) + ": ");
+			String name = myScanner.next();
+			Poll pollToAdd = new Poll(name, partyNamesArray.length);
+			for (String partyName : partyNamesArray) {
+				System.out.print("Enter the expected number of seats for " + partyName + ": ");
+				float expectedSeats = myScanner.nextFloat();
+				System.out.print("Enter the expected percentage of the vote for " + partyName + ": ");
+				float expectedVotes = myScanner.nextFloat();
+				pollToAdd.addParty(new Party(partyName, expectedSeats, expectedVotes));
+			}
+			this.polls.addPoll(pollToAdd);
+		}
+	}
+	
 	private void run() {
-		return;
+		System.out.println("Welcome to the poll tracker");
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.print("How many seats are available in the election? ");
+		int numOfSeats = myScanner.nextInt();
+		
+		System.out.println("Which parties are in the election (provide names, comma separated):");
+		String partyNames = myScanner.next();
+		String[] partyNamesArray = partyNames.split(",", -1);
+		
+		System.out.print("How many polls do you want to track with this application? ");
+		int numOfPolls = myScanner.nextInt();
+		
+		System.out.print("Would you like me to create a random set of polls? ");
+		String createRandomPolls = myScanner.next();
+		
+		
+		if (createRandomPolls.equalsIgnoreCase("yes")) {
+			Factory pollFactory = new Factory(numOfSeats);
+			pollFactory.setPartyNames(partyNamesArray);
+			this.polls = pollFactory.createRandomPollList(numOfPolls);
+		} else if (createRandomPolls.equalsIgnoreCase("no")) {
+			this.polls = new PollList(numOfPolls, numOfSeats);
+			promptForPollList(partyNamesArray);
+		}
+		
+		boolean running = true;
+		while (running) {
+			System.out.print("\nOptions: all (show result of all polls), aggregate (show aggregate result), "
+					+ "quit (end application) \nChoose an option: ");
+			String optionSelected = myScanner.next();
+			if (optionSelected.equalsIgnoreCase("aggregate")) {
+				displayPollDataBySeat(this.polls.getAggregatePoll(partyNamesArray));
+			} else if (optionSelected.equalsIgnoreCase("all")) {
+				displayPollsBySeat(partyNamesArray);
+			} else if (optionSelected.equalsIgnoreCase("quit")) {
+				return;
+			}
+		}
 	}
 	
 	
